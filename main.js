20c0e2cc4f926c73cc389b06ae743b4921c89467
Ruben Montero <rubenmontero@rocketmail.com>
2021-09-13 14:48:21 +0200

    Create an Account

---
+++ main.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnOpenModal = document.querySelectorAll(".submit");
+
+const openModal = function () {
+  if (document.querySelector(".input").value != "") {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  }
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+for (let i = 0; i < btnOpenModal.length; i++)
+  btnOpenModal[i].addEventListener("click", openModal);
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
+
+
